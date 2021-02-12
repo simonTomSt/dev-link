925e34f5c23f6ca9a7d3d8a6cffbf2d3bf2b11a4
Szymon Stepien <st.szymon.tom2001@gmail.com>
2021-02-12 20:07:36 +0100

    Add configuration with mongoDB

---
+++ server/config/db.js
@@ -0,0 +1,20 @@
+const config = require("config");
+const mongoose = require("mongoose");
+
+const db = config.get("mongoURI");
+const connectDB = async () => {
+  try {
+    console.log(db);
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("Mongo db connected");
+  } catch (err) {
+    console.log(err.message);
+
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;

ea85c8fc7d4e05dda01c7d112268114bbb8cc0fa
Szymon Stepien <st.szymon.tom2001@gmail.com>
2021-02-12 21:07:44 +0100

    Add UserSchema

---
+++ server/src/models/User.ts
@@ -0,0 +1,11 @@
+import { Type, createSchema, typedModel } from "ts-mongoose";
+
+const UserSchema = createSchema({
+  name: Type.string({ required: true }),
+  email: Type.string({ required: true, unique: true }),
+  password: Type.string({ required: true }),
+  avatar: Type.string(),
+  date: Type.date({ default: Date.now }),
+});
+
+export const User = typedModel("User", UserSchema);

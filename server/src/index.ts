31b2a9211092a9340d2fcb84d77f98f913773ae3
Szymon Stepien <st.szymon.tom2001@gmail.com>
2021-03-09 19:18:27 +0100

    Fix master

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,6 @@ import { authRoutes } from "./routes/api/auth";
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
-import path from "path";
 import { postsRoutes } from "./routes/api/posts";
 import { profileRoutes } from "./routes/api/profile";
 import { usersRoutes } from "./routes/api/users";
@@ -17,7 +16,11 @@ app.use(
   })
 );
 
+const PORT = process.env.PORT || 5000;
+
 connectDB();
+
+app.get("/", (req, res) => res.send("api working!"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
@@ -25,13 +28,4 @@ app.use("/api/profiles", profileRoutes);
 app.use("/api/posts", postsRoutes);
 app.use("/api/posts", postsRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server working"));
